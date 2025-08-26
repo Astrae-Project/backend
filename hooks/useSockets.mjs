d410d761638731dea5357834fdf178a8df0b0280
Raul <astraesystem@gmail.com>
2025-08-26 16:58:53 +0200

    Actualizar la URL del servidor de sockets a la dirección de producción

--- hooks/useSockets.mjs
+++ hooks/useSockets.mjs
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const SOCKET_SERVER_URL = "http://localhost:5000"; // Ajusta según corresponda
+const SOCKET_SERVER_URL = "https://backend-l3s8.onrender.com"; // Ajusta según corresponda
 
 const useSocket = (groupId) => {
   const [socket, setSocket] = useState(null);
