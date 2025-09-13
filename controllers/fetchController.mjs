2ce862146861840c0983f22abce95e33aa607fe7
Raul <astraesystem@gmail.com>
2025-09-13 17:25:11 +0200

    fetch pero startup

--- controllers/fetchController.mjs
+++ controllers/fetchController.mjs
@@ -365,7 +365,7 @@ export async function datosUsuario (req, res) {
 
             const eventosCreados = await prisma.evento.findMany({
                 where: {
-                    creador: { username: username },
+                    id_usuario: userId,
                 },
                 orderBy: { fecha_evento: 'asc' },
                 select: {
@@ -388,7 +388,7 @@ export async function datosUsuario (req, res) {
                 where: {
                     participantes: {
                         some: {
-                          usuario: { username: username },
+                          id_usuario: userId,
                         },
                     },
                 },
